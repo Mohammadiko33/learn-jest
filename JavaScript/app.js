25892afd716917d3b3fe0684ee4dacef2ef3a82b
developer-iko-mike <developer.iko.mike@gmail.com>
2025-08-29 18:21:03 +0330

    E10 : mock function part1

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,30 +1,13 @@
-function gte(num1, num2) {
-  return num1 >= num2 ? true : false;
+const { getUserById, sendEmail } = require("./service");
+
+function calculateDiscount(id) {
+  let discount = 0;
+  const { purchasesCount, name } = getUserById(id);
+  if (purchasesCount > 3) return (discount = 10);
+  sendEmail(name, `you have got %${discount} percent discount`);
+  return discount;
 }
-
-const welcomeMessage = (name) => `Hello ${name}. welcome to jest tutoral`;
-
-function shoppingList() {
-  return ["diseart", "cake", "cupcake"];
-}
-
-function getUser(){
-    return {
-        id: 1,
-        name: "mohammad"
-    }
-}
-
-const login = (password) => {
-  if (password !== "1234") throw new Error("Invalid password");
-  return { jwt: "GY@(YE(*@Y(!E*(&^!^E*&!^@E" };
-}
-
 
 module.exports = {
-  gte,
-  welcomeMessage,
-  shoppingList,
-  getUser,
-  login
+  calculateDiscount,
 };
--- JavaScript/tests/code.test.js
+++ JavaScript/tests/code.test.js
@@ -1,44 +1,22 @@
-const { gte, welcomeMessage, shoppingList, getUser, login } = require("../app");
+const service = require("../service")
+const app = require("../app")
 
-describe("gte", () => {
-  it("should return true if a is greater than b", () => {
-    const res = gte(10, 9);
-    expect(res).toBe(true); // درست است چون 10 > 9
-  });
-
-  it("should return false if a is less than b", () => {
-    const res = gte(5, 9);
-    expect(res).toBe(false); // درست است چون 5 < 9
-  });
-});
-
-describe("welcome message", () => {
-  it("should return a welcome message containing the name", () => {
-    const res = welcomeMessage("Mohammad");
-    expect(res).toContain("Mohammad");
-  });
-});
-
-describe("shopping List", () => {
-  it("array coffe shop ", () => {
-    const result = shoppingList();
-    expect(result).toContain("cupcake");
-  });
-});
-
-describe("get user", () => {
-  it("shoud username: mohammad", () => {
-    const res = getUser();
-    expect(res).toHaveProperty("name", "mohammad");
-  });
-});
-
-describe("login", () => {
-  it("should throw an error if password is wrrong", () => {
-    expect(() => login("1235")).toThrow()
-  });
-  it("should return jwt if password is not wrong" , () => {
-    const res = login("1234")
-    expect(res).toHaveProperty("jwt")
+describe("calculate discount" , () => {
+  it("should return 10 if user has more than 3 purchase", () => {
+    expect(async () => {
+      const mockFunction = jest.fn()
+      mockFunction.mockResolvedValue(12)
+      await mockFunction() // 12
+    })
   })
-});
+  it("should return 10 if user has more than 3 purchase", () => {
+    expect(() => {
+
+    })
+  })
+  it("should return 0 if user has more than 3 purchase", () => {
+    expect(() => {
+
+    })
+  })
+})
