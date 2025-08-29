123a933705ee6802d2c6239be91ffcc31a83d08f
developer-iko-mike <developer.iko.mike@gmail.com>
2025-08-29 14:19:42 +0330

    E5 : refactor project

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,7 +1,5 @@
 function gte(num1, num2) {
-  if (num1 > num2) return true;
-  if (num1 < num2) return false;
-  if (num1 === num2) return true;
+  return a >= b ? true : false;
 }
 
 module.exports = gte;
--- JavaScript/tests/code.test.js
+++ JavaScript/tests/code.test.js
@@ -1,16 +1,13 @@
 const gte = require("../app");
 
-test("gte - should return true if a is greater than b", () => {
+describe("gte", () => {
+  it("should return true if a is greater than b", () => {
     const res = gte(10, 9);
     expect(res).toBe(true); // درست است چون 10 > 9
-});
+  });
 
-test("gte - should return false if a is less than b", () => {
+  it("should return false if a is less than b", () => {
     const res = gte(5, 9);
     expect(res).toBe(false); // درست است چون 5 < 9
-});
-
-test("gte - should return true if a is equal to b", () => {
-    const res = gte(7, 7);
-    expect(res).toBe(true); // درست است چون 7 === 7
+  });
 });
